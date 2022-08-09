1c8054ad29da86493766025865e238aa35519eb4
João Matheus Silva Franca <matheussfranca@live.com>
2022-08-09 16:23:18 -0300

    Corrigindo erros de Lint

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -4,7 +4,7 @@ const fetchProducts = async (name) => {
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${name}`;
   const computer = await fetch(url)
   .then((response) => response.json())
-  .catch(() => {return new Error('You must provide an url')})
+  .catch(() => new Error('You must provide an url'));
   return computer;
 };
 
