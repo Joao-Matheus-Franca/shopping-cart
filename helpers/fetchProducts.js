dece517cca0e921d495a72be6ffcf28a77147aec
João Matheus Silva Franca <matheussfranca@live.com>
2022-08-09 16:19:23 -0300

    Concluído: Requisito 01

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,11 @@
-const fetchProducts = () => {
-  // seu código aqui
+// const fetch = require('node-fetch');
+
+const fetchProducts = async (name) => {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${name}`;
+  const computer = await fetch(url)
+  .then((response) => response.json())
+  .catch(() => {return new Error('You must provide an url')})
+  return computer;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,13 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testes da função fetchProducts', async () => {
+  const result = await fetchProducts('computador');
+  const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  expect(typeof fetchProducts).toEqual('function');
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch).toHaveBeenCalledWith(url);
+  expect(result).toEqual(computadorSearch);
+  expect(await fetchProducts()).toEqual(new Error('You must provide an url'));
+  })
 });
