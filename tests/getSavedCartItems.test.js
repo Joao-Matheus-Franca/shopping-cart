7f9fdf7c8c9dcf23e418ef7bde69a72a2d49b40f
João Matheus Silva Franca <matheussfranca@live.com>
2022-08-09 21:51:29 -0300

    Concluído: Requisito 07

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,4 +4,9 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
+    getSavedCartItems();
+    it('Testes da função getSavedCartItems', () => {
+      expect(localStorage.getItem).toHaveBeenCalledTimes(1);
+      expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+    })
 });
